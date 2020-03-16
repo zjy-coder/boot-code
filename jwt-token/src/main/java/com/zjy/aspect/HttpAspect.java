d09fc32aa9a94566f0b3b141b55f3ba7fe8840aa
TobeManager <2309839936@qq.com>
2020-03-16 16:31:16 +0800

    '提交'

---
+++ jwt-token/src/main/java/com/zjy/aspect/HttpAspect.java
@@ -0,0 +1,8 @@
+package com.zjy.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+@Aspect
+@Component
+public class HttpAspect {
+}
---
+++ jwt-token/src/test/java/com/zjy/JwtTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTokenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
